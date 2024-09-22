da19c76b7f5d883c5210cc185c7feade86d184f6
lmanju58 <lmanju58@gmail.com>
2024-09-22 23:30:47 +0530

    Removed the mysql congif as not required

--- cricket_game/settings.py
+++ cricket_game/settings.py
@@ -84,22 +84,13 @@ WSGI_APPLICATION = 'cricket_game.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cricket_game',
-        'USER': "root",
-        'PASSWORD':'Manju@123' ,
-        'HOST': 'localhost',  # Or the hostname of your MySQL server
-        'PORT': '3306',       # Or the port number of your MySQL server
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
